0144849e5e45cee46faf3ca61c86d3c179db435a
yuchen <kevin0101xiao@gmail.com>
2022-09-07 00:10:55 -0700

    Update display, quit, simulate inputs

--- MouseAutoMove/MouseAutoMove.cpp
+++ MouseAutoMove/MouseAutoMove.cpp
@@ -2,6 +2,8 @@
 
 #include "utilities.h"
 
+bool pos = false;
+
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
@@ -17,6 +19,10 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hwnd, &ps);
 
+        TextOut(hdc, 10, 10, TEXT("Double tap Space bar to exit!"), strlen("Double tap Space bar to exit!"));
+
+        ReleaseDC(hwnd, hdc);
+
         // All painting occurs here, between BeginPaint and EndPaint.
         FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
         EndPaint(hwnd, &ps);
@@ -30,16 +36,80 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
         GetWindowRect(hwnd, &rc);
         GetCursorPos(&pt);
 
-        if (pt.x == rc.left && pt.y == rc.top) 
+        if (pos) 
         {
-            SetCursorPos(rc.right, rc.bottom);
+            //SetCursorPos(rc.right, rc.bottom); 
+            
+            // long --> 0 ~ 65,535
+            // 
+            INPUT Inputs[5] = { 0 };
+
+            // Mouse move to middle
+            int nMaxWindowWidth = GetSystemMetrics(SM_CXMAXIMIZED);    
+            int nMaxWindowHeight = GetSystemMetrics(SM_CYMAXIMIZED);
+            int nDx = ((double)(rc.right)/2 + rc.left) / nMaxWindowWidth * 65535;
+            int nDy = ((double)(rc.bottom)/2 + rc.top) / nMaxWindowHeight * 65535;
+
+            Inputs[0].type = INPUT_MOUSE;
+            Inputs[0].mi.dx = nDx;//rc.right; // desired X coordinate
+            Inputs[0].mi.dy = nDy;//rc.bottom; // desired Y coordinate
+            Inputs[0].mi.dwFlags = MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE;
+
+            // Mouse left click -> noreact
+            Inputs[1].type = INPUT_MOUSE;
+            Inputs[1].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+
+            Inputs[2].type = INPUT_MOUSE;
+            Inputs[2].mi.dwFlags = MOUSEEVENTF_LEFTUP;
+
+
+            // Keyboard press -> noreact
+            Inputs[3].type = INPUT_KEYBOARD;
+            Inputs[3].ki.wVk = 0x41; //"A" key
+            //VK_SPACE to test
+
+            Inputs[4].type = INPUT_KEYBOARD;
+            Inputs[4].ki.wVk = 0x41;
+            Inputs[4].ki.dwFlags = KEYEVENTF_KEYUP;
+
+            SendInput(5, Inputs, sizeof(INPUT));
+            pos = !pos;
         }
         else 
         {
-            SetCursorPos(rc.left, rc.top);
+            //SetCursorPos(rc.left, rc.top);
+            int nMaxWindowWidth = GetSystemMetrics(SM_CXMAXIMIZED);
+            int nMaxWindowHeight = GetSystemMetrics(SM_CYMAXIMIZED);
+            int nDx = (double)(rc.left) / nMaxWindowWidth * 65535;
+            int nDy = (double)(rc.top) / nMaxWindowHeight * 65535;
+
+            INPUT Inputs[1] = { 0 };
+
+            // Mouse move
+            Inputs[0].type = INPUT_MOUSE;
+            Inputs[0].mi.dx = nDx;//rc.right; // desired X coordinate
+            Inputs[0].mi.dy = nDy;//rc.bottom; // desired Y coordinate
+            Inputs[0].mi.dwFlags = MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE;
+
+            SendInput(1, Inputs, sizeof(INPUT));
+            pos = !pos;
         }
         
     }
+
+    case WM_KEYDOWN:
+    {
+        if (wparam == VK_SPACE)
+        {
+            int res = MessageBox(nullptr, TEXT("You ensure want to stop it?"), TEXT("Exit"), MB_YESNO);
+
+            if (res == IDYES)
+            {
+                DestroyWindow(hwnd);
+            }
+        }
+    }
+
     return 0;
     }
 
@@ -70,10 +140,7 @@ int WINAPI wWinMain(_In_ HINSTANCE hinstance, _In_opt_ HINSTANCE, _In_ PWSTR p_c
         return 0;
     }
 
-
-    SetTimer(hwnd, ID_TIMER, 5000, 0);
-
-
+    SetTimer(hwnd, ID_TIMER, kTimerTime, 0);
     ShowWindow(hwnd, n_cmd_show);
 
     // Run the message loop.
@@ -88,14 +155,3 @@ int WINAPI wWinMain(_In_ HINSTANCE hinstance, _In_opt_ HINSTANCE, _In_ PWSTR p_c
 }
 
 
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- MouseAutoMove/utilities.h
+++ MouseAutoMove/utilities.h
@@ -6,4 +6,6 @@
 const wchar_t kClassName[] = L"Sample Window Class";	
 const wchar_t kWindowName[] = L"Mouse Auto Move";
 
+const int kTimerTime = 2000;
+
 void RegisterWindow(HINSTANCE hinstance, WNDPROC wndproc);
